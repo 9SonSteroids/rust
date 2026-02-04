0a6fce7f97c59ab23fcc41fb060cc380c0246ca6
jasper3108 <ich@yasupa.de>
2026-02-04 09:42:38 +0100

    fix 32 bit

--- tests/ui/reflection/trait_info_of_too_big.rs
+++ tests/ui/reflection/trait_info_of_too_big.rs
@@ -1,3 +1,4 @@
+//@ normalize-stderr: "\[u8; [0-9]+\]" -> "[u8; N]"
 //! Test for https://github.com/rust-lang/rust/pull/152003
 
 #![feature(type_info)]
@@ -5,15 +6,15 @@
 use std::any::TypeId;
 
 trait Trait {}
-impl Trait for [u8; 1 << 63] {}
+impl Trait for [u8; usize::MAX] {}
 
 fn main() {}
 
 const _: () = const {
-    TypeId::of::<[u8; 1 << 63]>().trait_info_of_trait_type_id(TypeId::of::<dyn Trait>());
-    //~^ ERROR values of the type `[u8; 9223372036854775808]` are too big for the target architecture [E0080]
+    TypeId::of::<[u8; usize::MAX]>().trait_info_of_trait_type_id(TypeId::of::<dyn Trait>());
+    //~^ ERROR values of the type `[u8; usize::MAX]` are too big for the target architecture
 };
 const _: () = const {
-    TypeId::of::<[u8; 1 << 63]>().trait_info_of::<dyn Trait>();
-    //~^ ERROR values of the type `[u8; 9223372036854775808]` are too big for the target architecture [E0080]
+    TypeId::of::<[u8; usize::MAX]>().trait_info_of::<dyn Trait>();
+    //~^ ERROR values of the type `[u8; usize::MAX]` are too big for the target architecture
 };
